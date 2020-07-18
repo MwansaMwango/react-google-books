1a90eb9db8d988f616a51bdff0fd0af020072871
Mwansa <Mwansad.mwango@gmail.com>
2020-07-18 23:04:25 +0800

    Updated server.js commentry

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+//Connect to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebook", { useNewUrlParser: true })
 
 app.listen(PORT, () => {
